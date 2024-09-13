d2b3989266f31c5f659577a6b261998f31afc390
tharinducypher <166396040+tharinducypher@users.noreply.github.com>
2024-09-13 13:10:34 +0530

    Revert "T-0001: Test commit 2"
    This reverts commit 08fd0e014096664b74f89077555a6e35d3430498.

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Test implementation 1");
-        System.out.println("Test implementation 2");
+        System.out.println("Test implementation 1");
 
     }
 }
